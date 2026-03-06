4ad168387221bf595d7a7f2f5596c2e51b4c3b5e
jslala222 <jslala222@gmail.com>
2026-03-06 18:36:45 +0900

    UI: Change recipes page to 2-column grid on mobile

--- src/app/recipes/page.tsx
+++ src/app/recipes/page.tsx
@@ -147,7 +147,7 @@ export default function RecipesPage() {
 
             {/* 레시피 그리드 */}
             {!isLoading && !error && filteredRecipes.length > 0 && (
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+                <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 gap-3">
                     {filteredRecipes.map(recipe => (
                         <RecipeCard key={recipe.id} recipe={recipe} />
                     ))}
